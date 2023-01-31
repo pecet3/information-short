860f1c6033718ecd37763132bc26650e1d2e3dce
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-01 00:44:04 +0100

    deleted files

--- src/features/main/styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const MajorWrapper = styled.div`
-    max-width: 1200px;
-    padding: 0px 20px 30px 20px;
-    margin: 0 auto;
-    text-align: center;
-`;
