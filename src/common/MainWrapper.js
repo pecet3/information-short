f27a3c078e3c748a823c02a52a922712eb898063
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-01 00:45:05 +0100

    edited margin

--- src/common/MainWrapper.js
+++ src/common/MainWrapper.js
@@ -4,7 +4,7 @@ export const MainWrapper = styled.main`
     background-color: ${({ theme }) => theme.elements.bigTile};
     text-align: left;
     padding: 10px;
-    margin-top: 20px;
+    margin-top: 10px;
     display: grid;
     grid-template-columns: 1fr 1fr;
     grid-gap: 10px;
