1c2cfae3f9b71e1141f7e4080e16547fb772c226
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-31 22:31:34 +0100

    created store

---
+++ src/store.js
@@ -0,0 +1,17 @@
+import createSagaMiddleware from "@redux-saga/core";
+import { configureStore } from "@reduxjs/toolkit";
+import newsReducer from "./features/Main/News/newsSlice";
+import rootSaga from "./rootSaga";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = configureStore({
+    reducer: {
+        news: newsReducer,
+    },
+    middleware: [sagaMiddleware],
+});
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
