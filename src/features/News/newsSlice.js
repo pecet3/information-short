e80abfb1aea01e9b943028a7ca2d756ae5dad23c
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-01 00:46:21 +0100

    created slice

---
+++ src/features/News/newsSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const newsSlice = createSlice({
+    name: "news",
+    initialState: {
+        news: [],
+        status: "initial",
+    },
+    reducers: {
+        fetchNews: (state) => {
+            state.status = "loading"
+        },
+        fetchNewsSuccess: (state, { payload: news }) => {
+            state.status = "success"
+            state.news = news
+        },
+    }
+});
+
+export const {
+    fetchNews,
+    fetchNewsSuccess } = newsSlice.actions;
+
+export const selectNewsState = (state) => state.news;
+
+export const selectNews = (state) => selectNewsState(state).news.results;
+export const selectStatus = (state) => selectNewsState(state).status;
+
+export default newsSlice.reducer;
