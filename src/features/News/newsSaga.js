c003069678ee8c0c3bd3eeccb18788c811a1ecde
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-01 00:46:12 +0100

    created saga

---
+++ src/features/News/newsSaga.js
@@ -0,0 +1,24 @@
+import {
+    call,
+    delay,
+    takeLatest,
+    put
+} from "@redux-saga/core/effects";
+import { getNews } from "./getNews";
+import { fetchNews, fetchNewsSuccess } from "./newsSlice";
+
+function* fetchNewsHandler() {
+    try {
+        const news = yield call(getNews);
+        yield put(fetchNewsSuccess(news));
+    } catch (error) {
+        yield call(console.error(error));
+    }
+};
+
+export function* newsSaga() {
+    yield takeLatest(fetchNews.type, fetchNewsHandler);
+};
+
+
+
