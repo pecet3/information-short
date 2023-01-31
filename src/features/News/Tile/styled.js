6ffa4a3bd0833f5b886218db52c083bf4d7c7d4b
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-01 00:47:16 +0100

    styled Tile

---
+++ src/features/News/Tile/styled.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    background-color: ${({ theme }) => theme.elements.tile};
+    display: flex;
+    flex-direction: column;
+    padding: 10px;
+    border-radius: ${({ theme }) => theme.props.borderRadius}px;
+`;
+export const Header = styled.div`
+    display: flex;
+`;
+export const TitleWrapper = styled.div`
+`;
+export const Title = styled.h2`
+    font-size: 20px;
+    margin: 0;
+    color:${({ theme }) => theme.elements.textImportant};
+`;
+export const Image = styled.img`
+    width: 200px;
+    height: auto;
+    margin-right: 10px;
+    border-radius: ${({ theme }) => theme.props.borderRadius}px;
+    flex-basis: 1;
+`;
+export const Date = styled.p`
+    font-size: 12px;
+    margin: 5px 0 10px 0;
+    font-weight: 100;
+`;
+export const Description = styled.p`
+    font-size: 14px;
+    margin: 10px 0 0 0;
+`;
