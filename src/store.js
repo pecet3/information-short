fe9f5f455e7faa4bba482426b5a9edd54aebe015
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-01 00:44:46 +0100

    changed path to reducer

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import createSagaMiddleware from "@redux-saga/core";
 import { configureStore } from "@reduxjs/toolkit";
-import newsReducer from "./features/Main/News/newsSlice";
+import newsReducer from "./features/News/newsSlice";
 import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
