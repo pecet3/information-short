d79de1ff8aa7a9832aacef4a0c90a381d3a92c74
Jakub Pacewicz <pecet3107@gmail.coim>
2023-01-31 22:32:04 +0100

    styled component

--- src/StyledApp.js
+++ src/StyledApp.js
@@ -3,4 +3,8 @@ import styled from "styled-components";
 export const StyledApp = styled.div`
     background-color: ${({ theme }) => theme.colors.mineshaft};
     font-family: 'Poppins', sans-serif;
+    text-align: center;
+    margin: 0 auto;
+    max-width: 1200px;
+    padding: 0 10px 10px 10px;
 `;
