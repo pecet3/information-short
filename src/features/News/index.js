2a8ad4a25ee0b922f1f095accf96992fffd3c54c
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-01 00:45:52 +0100

    replaced News

---
+++ src/features/News/index.js
@@ -0,0 +1,32 @@
+import { nanoid } from "@reduxjs/toolkit";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { MainWrapper } from "../../common/MainWrapper";
+import { fetchNews, selectNews, selectStatus } from "./newsSlice";
+import { Tile } from "./Tile";
+
+
+export const News = () => {
+    const news = useSelector(selectNews);
+    const status = useSelector(selectStatus);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchNews())
+    }, [dispatch]);
+
+    return (
+        <>
+            <MainWrapper>
+                {status === "success" && news.map((element) =>
+                    <Tile as="li"
+                        key={nanoid()}
+                        title={element.title}
+                        image={element.image_url}
+                        date={element.pubDate}
+                        description={element.description}
+                    />)}
+            </MainWrapper>
+        </>
+    )
+};
