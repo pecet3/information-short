5c8fd6ebdd4d1d8501f8cbae37f35c47aac7c294
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-02 10:39:10 +0100

    logic for render components

--- src/features/News/index.js
+++ src/features/News/index.js
@@ -1,6 +1,8 @@
 import { nanoid } from "@reduxjs/toolkit";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Error } from "../../common/Error";
+import { Loading } from "../../common/Loading";
 import { MainWrapper } from "../../common/MainWrapper";
 import { fetchNews, selectNews, selectStatus } from "./newsSlice";
 import { Tile } from "./Tile";
@@ -18,14 +20,18 @@ export const News = () => {
     return (
         <>
             <MainWrapper>
-                {status === "success" && news.map((element) =>
-                    <Tile as="li"
-                        key={nanoid()}
-                        title={element.title}
-                        image={element.image_url}
-                        date={element.pubDate}
-                        description={element.description}
-                    />)}
+                {status === "error"
+                    ? <Error />
+                    : status === "initial" || status === "loading"
+                        ? <Loading />
+                        : news.map((element) =>
+                            <Tile as="li"
+                                key={nanoid()}
+                                title={element.title}
+                                image={element.image_url}
+                                date={element.pubDate}
+                                description={element.description}
+                            />)}
             </MainWrapper>
         </>
     )
