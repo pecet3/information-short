0a8f336a0da97b2317ff9be3e57ab07ccdd9c8d0
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-02 14:06:19 +0100

    noRender property in Wrapper, @media in components for responsive

--- src/features/News/Tile/styled.js
+++ src/features/News/Tile/styled.js
@@ -1,32 +1,45 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Wrapper = styled.div`
     background-color: ${({ theme }) => theme.elements.tile};
     display: flex;
     flex-direction: column;
+    justify-content: space-between;
     padding: 10px;
     border-radius: ${({ theme }) => theme.props.borderRadius}px;
+
+    ${({ noRender }) => noRender && css`
+        display: none;
+    `}
 `;
 export const Header = styled.div`
     display: flex;
 
-    @media (max-width:${({ theme }) => theme.breakpoints.mobile}px){
+    @media (max-width:${({ theme }) => theme.breakpoints.large}px){
         flex-direction: column;
     }
 
 `;
 export const TitleWrapper = styled.div`
 `;
+
 export const Title = styled.h2`
     font-size: 20px;
     margin: 0;
     color:${({ theme }) => theme.elements.textImportant};
+
+    @media (max-width:${({ theme }) => theme.breakpoints.large}px){
+        margin-top: 10px;
+    }
 `;
 export const Image = styled.img`
     width: 200px;
     height: auto;
     margin-right: 10px;
     flex-basis: 1;
+    @media (max-width:${({ theme }) => theme.breakpoints.large}px){
+        width: 100%;
+    }
 `;
 export const Date = styled.p`
     font-size: 12px;
