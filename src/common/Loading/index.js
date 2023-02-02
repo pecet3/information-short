dbf4d815f63e72eab78bd448f8566225ca1cfce9
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-02 10:38:04 +0100

    created module

---
+++ src/common/Loading/index.js
@@ -0,0 +1,9 @@
+import { Title, Wrapper } from "./styled"
+
+export const Loading = () => {
+    return (
+        <Wrapper>
+            <Title>Loading...</Title>
+        </Wrapper>
+    )
+};
---
+++ src/common/Loading/styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    height: 100%;
+
+`;
+
+export const Title = styled.h2`
+
+`;
+
+export const Subtitle = styled.h3`
+
+`;
