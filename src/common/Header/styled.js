6e4f75e3959d4b5d7f364aa4d6195d511b6d42fc
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-03 01:56:06 +0100

    added font, letter-spacing and responsive

--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -3,10 +3,21 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
     text-align: left;
     background-color: ${({ theme }) => theme.elements.headerBackground};
-    padding: 20px;
+    padding: 24px;
+    font-family: 'Roboto Mono', monospace;
+
+    @media (max-width:${({ theme }) => theme.breakpoints.mobile}px){
+        padding: 10px 10px;
+    }
 `;
 
 export const Title = styled.h1`
     color: ${({ theme }) => theme.elements.header};
+    letter-spacing: 0.1em;
     margin: 0;
+
+    @media (max-width:${({ theme }) => theme.breakpoints.mobile}px){
+        font-size: 22px;
+    }
+    
 `;
