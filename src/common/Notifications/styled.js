45e8f752a37b0b87e3c95489b6962cf7c8ec902e
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-03 01:56:58 +0100

    styled Icon and sSpinner

--- src/common/Notifications/styled.js
+++ src/common/Notifications/styled.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
-
 export const Wrapper = styled.div`
     height: 100%;
     text-align: center;
+    margin-top: 60px;
+    margin-bottom: 70%;
 `;
 
 export const Title = styled.h2`
@@ -11,4 +12,16 @@ export const Title = styled.h2`
 
 export const Subtitle = styled.h3`
 
+`;
+
+export const Icon = styled.img`
+    height: 72px;
+    width: auto;
+`;
+
+export const Spinner = styled.img`
+    margin: 0;
+    height: 100px;
+    width: auto;
+  
 `;
