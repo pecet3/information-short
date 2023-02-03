f774d9b894c3f13dece5d5760b141a46f0073b72
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-03 01:55:15 +0100

    added responsive for component

--- src/StyledApp.js
+++ src/StyledApp.js
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 
 export const StyledApp = styled.div`
-    background-color: ${({ theme }) => theme.colors.mineshaft};
+    background-color: transparent;
     font-family: 'Poppins', sans-serif;
     text-align: center;
     margin: 0 auto;
     max-width: 1200px;
     padding: 0 10px 10px 10px;
+
+    @media (max-width:${({ theme }) => theme.breakpoints.mobile}px){
+        padding: 0 5px 5px 5px;
+    }
 `;
