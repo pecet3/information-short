db9a6ed42e723cb336ef68ca12b21f31718b9bc9
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-04 02:22:32 +0100

    added flex to Wrapper, created Clock

--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -5,6 +5,10 @@ export const Wrapper = styled.div`
     background-color: ${({ theme }) => theme.elements.headerBackground};
     padding: 24px;
     font-family: 'Roboto Mono', monospace;
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+    flex-wrap: wrap;
 
     @media (max-width:${({ theme }) => theme.breakpoints.mobile}px){
         padding: 10px 10px;
@@ -20,4 +24,14 @@ export const Title = styled.h1`
         font-size: 22px;
     }
     
+`;
+
+export const Clock = styled.p`
+    color: ${({ theme }) => theme.elements.text};
+    margin: 0;
+
+    @media (max-width:${({ theme }) => theme.breakpoints.mobile}px){
+        font-size: 12px;
+        margin-top: 5px;
+    }
 `;
