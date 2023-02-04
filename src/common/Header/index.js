69901f701d59d7c4bf167b5de3402e5e9542b641
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-04 02:22:10 +0100

    added Clock to Header

--- src/common/Header/index.js
+++ src/common/Header/index.js
@@ -1,9 +1,39 @@
-import { Title, Wrapper } from "./styled"
+import { useEffect, useState } from "react";
+import { Title, Wrapper, Clock } from "./styled"
+
+
+const useCurrentDate = () => {
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date());
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    return date;
+}
+
 
 export const Header = () => {
+    const date = useCurrentDate();
     return (
         <Wrapper as="header">
             <Title>Informacja krótka.</Title>
+            <Clock>{date.toLocaleDateString(undefined, {
+                weekday: "long",
+                day: "numeric",
+                month: "long",
+                year: "numeric",
+                hour: "numeric",
+                minute: "numeric",
+                second: "numeric",
+            })}
+            </Clock>
         </Wrapper>
     )
 }
