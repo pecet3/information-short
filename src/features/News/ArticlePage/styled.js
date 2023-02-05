9e51d4eb77bcfbbee0ddd87eb8876806f2da2d52
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-05 02:11:15 +0100

    styled for responsive

--- src/features/News/ArticlePage/styled.js
+++ src/features/News/ArticlePage/styled.js
@@ -3,20 +3,29 @@ import styled from "styled-components";
 export const Article = styled.article`
     display: flex;
     flex-direction: column;
+    word-break: break-word;
 `;
 export const Header = styled.header`
 
 `;
 
 export const Title = styled.h2`
-    margin: 5px 0 10px 0;
+    margin: 5px 0 5px 0;
     color: ${({ theme }) => theme.elements.textImportant};
+
+    @media (max-width:${({ theme }) => theme.breakpoints.mobile}px) {
+        font-size: 18px;
+    }
 `;
 
 export const Date = styled.p`
-    margin: 0;
-    font-size: 14px;
+    margin: 0 0 20px 0;
+    font-size: 12px;
     font-weight: 100;
+
+    @media (max-width:${({ theme }) => theme.breakpoints.mobile}px) {
+        font-size: 10px;
+    }
 
 `;
 
@@ -26,7 +35,10 @@ export const Image = styled.img`
 `;
 
 export const Content = styled.p`
-
+    line-height: 1.6;
+    @media (max-width:${({ theme }) => theme.breakpoints.mobile}px) {
+            font-size: 14px;
+        }
 `;
 
 export const Source = styled.a`
