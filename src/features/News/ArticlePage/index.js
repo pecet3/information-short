47d13946dcde232348bb747ffba9cf55c95334d3
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-05 02:10:58 +0100

    added Error and Loading. Added selectStatus for conditions to render components

--- src/features/News/ArticlePage/index.js
+++ src/features/News/ArticlePage/index.js
@@ -1,38 +1,44 @@
 import { useSelector } from "react-redux";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { MainWrapper } from "../../../common/MainWrapper"
-import { getArticleByDate } from "../newsSlice";
+import { Error } from "../../../common/Notifications/Error";
+import { Loading } from "../../../common/Notifications/Loading";
+import { getArticleByName, selectStatus } from "../newsSlice";
 import { Article, Content, Date, Header, Image, Source, Title } from "./styled";
 
 export const ArticlePage = () => {
     const { id } = useParams();
-    const article = useSelector(state => getArticleByDate(state, id))
+    const article = useSelector(state => getArticleByName(state, id))
+    const status = useSelector(selectStatus);
+
     return (
         <MainWrapper oneColumn={true}>
-            {article &&
-                <>
-                    <Article>
+            {status === "error"
+                ? <Error />
+                : status === "initial"
+                    || status === "loading"
+                    ? <Loading />
+                    : <Article>
                         <Header>
                             <Image
-                                src={article.image_url}
+                                src={article.image_url && article.image_url}
                                 alt="zdjęcie do artykułu"
                             />
-                            <Title>{article.title}</Title>
-                            <Date>{article.pubDate}</Date>
+                            <Title>{article.title && article.title}</Title>
+                            <Date>{article.pubDate && article.pubDate}</Date>
                         </Header>
-                        <Content>{article.description}</Content>
-                        <Content>{article.content}</Content>
+                        <Content>{article.description && article.description}</Content>
+                        <Content>{article.content && article.content}</Content>
                         <Content>
                             Źródło: <Source
-                                href={article.link}
+                                href={article.link && article.link}
                                 target="_blank"
                                 rel="noreferrer"
                             >
-                                {article.link}
+                                {article.link && article.link}
                             </Source>
                         </Content>
-                    </Article>
-                </>}
+                    </Article>}
         </MainWrapper>
     )
 }
