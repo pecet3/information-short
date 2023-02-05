f6d55c3ce2198bc0de64aa4d241af11209843cb1
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-05 01:03:31 +0100

    edited and added routes

--- src/routes.js
+++ src/routes.js
@@ -1,2 +1,4 @@
-export const toMain = () => "/";
+export const toNews = () => "/news";
 export const toWeather = () => "/pogoda";
+
+export const toArticle = ({ id } = { id: ":id" }) => `/news/${id}`;
