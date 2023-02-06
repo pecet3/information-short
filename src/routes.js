3c55b2732770f4386a5bc497e1051abdc50652a3
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-06 23:01:07 +0100

    changed route

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 export const toNews = () => "/news";
 export const toWeather = () => "/pogoda";
-export const toDogs = () => "/dogs";
+export const toDogs = () => "/pieski";
 
 export const toArticle = ({ id } = { id: ":id" }) => `/news/${id}`;
