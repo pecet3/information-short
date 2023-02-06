b4d7b246e9f4ca52108895a89896bcf91d94bd69
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-06 19:40:09 +0100

    added route

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 export const toNews = () => "/news";
 export const toWeather = () => "/pogoda";
+export const toDogs = () => "/dogs";
 
 export const toArticle = ({ id } = { id: ":id" }) => `/news/${id}`;
