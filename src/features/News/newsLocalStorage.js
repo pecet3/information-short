f08cf585d718e36afbbd0ca9eb69e89e6e86ddec
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-06 23:05:34 +0100

    removed localStorage

--- src/features/News/newsLocalStorage.js
+++
@@ -1,7 +0,0 @@
-const localStorageKey = "news";
-
-export const saveNewsInLocalStorage = (news) =>
-    localStorage.setItem(localStorageKey, JSON.stringify(news));
-
-export const getNewsFromLocalStorage = () =>
-    JSON.parse(localStorage.getItem(localStorageKey)) || [];
