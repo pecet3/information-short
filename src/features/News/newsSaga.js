1cde4a5f37effa4776151422051ec8b74c6bf87c
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-06 23:05:56 +0100

    removed saveNewsInLocalStorage

--- src/features/News/newsSaga.js
+++ src/features/News/newsSaga.js
@@ -2,10 +2,8 @@ import {
     call,
     takeLatest,
     put,
-    takeEvery
 } from "@redux-saga/core/effects";
 import { getNews } from "./getNews";
-import { saveNewsInLocalStorage } from "./newsLocalStorage";
 import { fetchNews, fetchNewsError, fetchNewsSuccess } from "./newsSlice";
 
 function* fetchNewsHandler() {
@@ -13,19 +11,13 @@ function* fetchNewsHandler() {
         const news = yield call(getNews);
         yield put(fetchNewsSuccess(news));
     } catch (error) {
-        yield put(fetchNewsError(error));
+        yield put(fetchNewsError());
         yield call(console.error(error));
     }
 };
 
-function* saveNewsInLocalStorageHandler() {
-    const news = yield call(getNews);
-    yield call(saveNewsInLocalStorage, news);
-}
-
 export function* newsSaga() {
     yield takeLatest(fetchNews.type, fetchNewsHandler);
-    yield takeEvery("*", saveNewsInLocalStorageHandler)
 };
 
 
