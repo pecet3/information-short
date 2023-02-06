beac495040af33890058b1ffa4b22f6e89b1f37c
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-06 23:02:23 +0100

    added Text with content

--- src/common/Notifications/Loading/index.js
+++ src/common/Notifications/Loading/index.js
@@ -1,4 +1,4 @@
-import { Spinner, Subtitle, Title, Wrapper } from "../styled";
+import { Spinner, Subtitle, Text, Title, Wrapper } from "../styled";
 import LoadingSpinner from "./spinner.svg";
 
 export const Loading = () => {
@@ -7,6 +7,9 @@ export const Loading = () => {
             <Spinner src={LoadingSpinner} alt="Ikonka ładowania" />
             <Title>Trwa ładowanie...</Title>
             <Subtitle>Prosimy o chwilę cierpliwości</Subtitle>
+            <Text>
+                Jeśli ładowanie trwa zbyt długo, proszę odświeżyć stronę.
+            </Text>
         </Wrapper>
     )
 };
