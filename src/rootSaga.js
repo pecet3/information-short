de7d13d998af9e6352548283b6dd8f78d5d93837
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-06 19:40:22 +0100

    added new Saga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,8 +1,10 @@
 import { all } from "@redux-saga/core/effects";
+import { dogsSaga } from "./features/Dogs/dogsSaga";
 import { newsSaga } from "./features/News/newsSaga";
 
 export default function* rootSaga() {
     yield all([
         newsSaga(),
+        dogsSaga(),
     ]);
 };
