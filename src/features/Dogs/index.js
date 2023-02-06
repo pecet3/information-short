f71112954b832195032d9e62d720d143f189d3e3
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-06 23:04:18 +0100

    added Button

--- src/features/Dogs/index.js
+++ src/features/Dogs/index.js
@@ -1,22 +1,40 @@
-import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { MainWrapper } from "../../common/MainWrapper.js"
+import { MainWrapper } from "../../common/MainWrapper.js";
 import { Error } from "../../common/Notifications/Error/index.js";
 import { Loading } from "../../common/Notifications/Loading/index.js";
-import { fetchDogs, selectDogs, selectDogsStatus } from "./dogsSlice.js";
-import { Image } from "./styled.js";
+import {
+    fetchDogs,
+    selectDogs,
+    selectDogsStatus,
+} from "./dogsSlice.js";
+import {
+    Button,
+    ButtonIcon,
+    Image,
+    ButtonText,
+} from "./styled.js";
+import icon from "./icon.svg";
+import { useEffect } from "react";
+
+
 export const Dogs = () => {
     const dispatch = useDispatch();
     const dogs = useSelector(selectDogs);
     const status = useSelector(selectDogsStatus);
 
-    useEffect(() => {
-        dispatch(fetchDogs());
-    }, [dispatch])
 
+    useEffect(() => {
+        dispatch(fetchDogs())
+    }, [dispatch])
 
     return (
         <MainWrapper oneColumn={true}>
+            {status === "success" &&
+                <Button onClick={() => dispatch(fetchDogs())}>
+                    <ButtonIcon src={icon} alt="ikonka przeładowania" />
+                    <ButtonText>Załaduj nowego pieska</ButtonText>
+                </Button>
+            }
             {status === "initial"
                 || status === "loading"
                 ? <Loading />
