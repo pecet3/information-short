7148f0b757766053d1e948ce4f8c91e25ef87f7e
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-08 22:17:59 +0100

    edited path in import

---
+++ src/features/Animals/Cats/index.js
@@ -0,0 +1,9 @@
+import { MainWrapper } from "../../../common/MainWrapper"
+
+export const Cats = () => {
+    return (
+        <MainWrapper>
+            <p>Sekcja w budowie</p>
+        </MainWrapper>
+    )
+}
