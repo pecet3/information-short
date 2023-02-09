628285f4ea030cab10f2523238eafaf09cd0da27
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-09 21:22:31 +0100

    added new route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Header } from './common/Header';
 import { Navigation } from './common/Navigation';
+import { Cats } from './features/Cats';
 import { Dogs } from './features/Dogs';
 import { News } from './features/News';
 import { ArticlePage } from './features/News/ArticlePage';
 import { Weather } from './features/Weather';
 import { GlobalStyle } from './GlobalStyle';
-import { toArticle, toDogs, toNews, toWeather } from './routes';
+import { toArticle, toCats, toDogs, toNews, toWeather } from './routes';
 import { StyledApp, StickyContainer } from './StyledApp';
 import { theme } from './theme';
 
@@ -34,6 +35,9 @@ function App() {
             </Route>
             <Route path={toDogs()}>
               <Dogs />
+            </Route>
+            <Route path={toCats()}>
+              <Cats />
             </Route>
             <Route path="/">
               <Redirect to={toNews()} />
