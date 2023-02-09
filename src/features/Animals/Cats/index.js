8b31cbac585e53e7140596d22a3702375a6d2566
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-09 18:25:44 +0100

    deleted folder

--- src/features/Animals/Cats/index.js
+++
@@ -1,9 +0,0 @@
-import { MainWrapper } from "../../../common/MainWrapper"
-
-export const Cats = () => {
-    return (
-        <MainWrapper>
-            <p>Sekcja w budowie</p>
-        </MainWrapper>
-    )
-}
