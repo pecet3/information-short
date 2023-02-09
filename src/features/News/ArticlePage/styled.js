81c0cae2da5382d6134a2232b28b565942203dcc
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-09 18:22:12 +0100

    enlarged font by 2px, enlarged line-height in Content

--- src/features/News/ArticlePage/styled.js
+++ src/features/News/ArticlePage/styled.js
@@ -12,19 +12,20 @@ export const Header = styled.header`
 export const Title = styled.h2`
     margin: 5px 0 5px 0;
     color: ${({ theme }) => theme.elements.textImportant};
+    font-size: 22px;
 
     @media (max-width:${({ theme }) => theme.breakpoints.mobile}px) {
-        font-size: 18px;
+        font-size: 20px;
     }
 `;
 
 export const Date = styled.p`
     margin: 0 0 20px 0;
-    font-size: 12px;
+    font-size: 14px;
     font-weight: 100;
 
     @media (max-width:${({ theme }) => theme.breakpoints.mobile}px) {
-        font-size: 10px;
+        font-size: 12px;
     }
 
 `;
@@ -35,9 +36,10 @@ export const Image = styled.img`
 `;
 
 export const Content = styled.p`
-    line-height: 1.6;
+    line-height: 1.8;
+
     @media (max-width:${({ theme }) => theme.breakpoints.mobile}px) {
-            font-size: 14px;
+            font-size: 16px;
         }
 `;
 
