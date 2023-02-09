10ec83801b8235fcd757ee67a6fb741612167307
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-09 21:25:58 +0100

    built section

--- src/features/Cats/index.js
+++ src/features/Cats/index.js
@@ -1,9 +1,39 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { BigImage } from "../../common/BigImage";
 import { MainWrapper } from "../../common/MainWrapper"
+import { Error } from "../../common/notifications/Error";
+import { Loading } from "../../common/notifications/Loading";
+import { RefreshButton } from "../../common/RefreshButton";
+import { fetchCats, selectCatsStatus, selectCats } from "./catsSlice";
 
 export const Cats = () => {
+    const dispatch = useDispatch();
+    const status = useSelector(selectCatsStatus);
+    const cats = useSelector(selectCats)
+
+    useEffect(() => {
+        dispatch(fetchCats())
+    }, [dispatch])
+
+
     return (
-        <MainWrapper>
-            <p>Sekcja w budowie</p>
+        <MainWrapper oneColumn={true}>
+            {status !== "initial" &&
+                <RefreshButton
+                    buttonText="Załaduj nowego kotka"
+                    action={() => dispatch(fetchCats())}
+                />
+            }
+            {status === "initial"
+                || status === "loading"
+                ? <Loading />
+                : status === "error"
+                    ? <Error />
+                    : <BigImage
+                        src={cats.file}
+                        alt="zdjęcie kotka"
+                    />}
         </MainWrapper>
     )
 }
