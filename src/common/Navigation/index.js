39e036053b7187c4d06d2b78cd01edbc8bc79354
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-13 22:48:15 +0100

    removed: Ul, Li for News and Pogoda, allattributes

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,31 +1,36 @@
-import { toNews, toWeather, toDogs, toCats, toPets } from "../../routes";
-import { DropDownLink, DropDownContainer, Li, DropDownList, StyledNavLink, Ul, Wrapper } from "./styled";
+import {
+    toNews,
+    toWeather,
+    toDogs,
+    toCats,
+    toPets
+} from "../../routes";
+import {
+    DropDownLink,
+    DropDownContainer,
+    Li,
+    DropDownList,
+    StyledNavLink
+    , Wrapper
+} from "./styled";
 
 
 export const Navigation = () => {
     return (
         <Wrapper>
-            <Ul>
-                <Li>
-                    <StyledNavLink to={toNews()}>News</StyledNavLink>
-                </Li>
-                <Li>
-                    <StyledNavLink to={toWeather()}>Pogoda</StyledNavLink>
-                </Li>
-                <Li>
-                    <DropDownContainer>
-                        <DropDownLink to={toPets()}>Zwierzęta</DropDownLink>
-                        <DropDownList>
-                            <Li displayUpright={true.toString()}>
-                                <DropDownLink to={toDogs()} upright={true.toString()}>Pieski</DropDownLink>
-                            </Li>
-                            <Li displayUpright={true}>
-                                <DropDownLink to={toCats()} upright={true.toString()}>Kotki</DropDownLink>
-                            </Li>
-                        </DropDownList>
-                    </DropDownContainer>
-                </Li>
-            </Ul>
+            <StyledNavLink to={toNews()}>News</StyledNavLink>
+            <StyledNavLink to={toWeather()}>Pogoda</StyledNavLink>
+            <DropDownContainer>
+                <StyledNavLink to={toPets()}>Zwierzęta</StyledNavLink>
+                <DropDownList>
+                    <Li>
+                        <DropDownLink to={toDogs()}>Pieski</DropDownLink>
+                    </Li>
+                    <Li>
+                        <DropDownLink to={toCats()}>Kotki</DropDownLink>
+                    </Li>
+                </DropDownList>
+            </DropDownContainer>
         </Wrapper>
     )
 };
