d7546a840e5ec2c64dc7fb36eb6870ec405057a3
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-13 22:49:20 +0100

    removed Ul, edited every components

--- src/common/Navigation/styled.js
+++ src/common/Navigation/styled.js
@@ -1,32 +1,12 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 
 export const Wrapper = styled.nav`
     background-color: ${({ theme }) => theme.elements.navigationBackground};
-`;
-
-export const Ul = styled.ul`
     display: flex;
-    list-style: none;
     margin: 0;
     padding: 0;
-`;
-
-export const Li = styled.li`
-    padding: 10px;
-    text-align: left;
-    &:hover{
-        background-color: ${({ theme }) => theme.elements.navigationBackgroundHover};
-    }
-
-    ${({ displayUpright }) => displayUpright && css`
-        padding: 8px 10px;
-
-        &:hover{
-            background-color: ${({ theme }) => theme.elements.navigationBackgroundHover + 50};
-    }
-    `}
 `;
 
 export const StyledNavLink = styled(NavLink)`
@@ -35,7 +15,11 @@ export const StyledNavLink = styled(NavLink)`
     letter-spacing: 0.1em;
     color: ${({ theme }) => theme.elements.navigationText};
     transition: color 0.4s;
-    
+    padding: 5px 10px;
+
+    &:hover{
+        background-color: ${({ theme }) => theme.elements.navigationBackgroundHover};
+    }
     &:visited{
         text-decoration: none;
         color: ${({ theme }) => theme.elements.navigationText};
@@ -47,36 +31,50 @@ export const StyledNavLink = styled(NavLink)`
 `;
 
 export const DropDownContainer = styled.div`
+    display: flex;
     position: relative;
+
     &:hover{
         cursor: pointer;
     }
 `;
 
 export const DropDownLink = styled(StyledNavLink)`
-    padding-bottom: 9px;
-
-    ${({ upright }) => upright && css`
-
+    color: ${({ theme }) => theme.elements.text};
+    width: 100%;
+    height: 100%;
+    display: inline-flex;
+    padding: 5px 10px;
+    margin: 0;
+    &:hover{
+        background-color: transparent;
+    }
+    &:visited{
         color: ${({ theme }) => theme.elements.text};
-        &:visited{
-            color: ${({ theme }) => theme.elements.text};
-        }
-    `}
+    }
 `;
+export const Li = styled.li`
+    text-align: left;
+    &:hover{
+        background-color: ${({ theme }) => theme.elements.navigationBackgroundHover + 50}
+    }
+`;
+
 
 export const DropDownList = styled.ul`
     position: absolute;
     list-style: none;
     border-radius: 0 0 5px 5px;
     padding: 0;
-    width: 115px;
-    right: -11%;
-    top: 155%;
+    margin-top: 28.5px;
+    width: 114.5px;
     display: none;
     background-color: ${({ theme }) => theme.elements.headerBackground};
     
-    ${DropDownContainer}:hover & {
+    ${DropDownContainer}:hover & {  
+        display: block;
+    }
+    ${DropDownLink}:focus & {
         display: block;
     }
 `;
