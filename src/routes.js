635a0e8a594a25b1406f82b0a0f56fe2e045afe6
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-13 01:07:49 +0100

    edited toCats route

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,6 @@ export const toNews = () => "/news";
 export const toWeather = () => "/pogoda";
 export const toPets = () => "/zwierzeta";
 export const toDogs = () => "/zwierzeta/pieski";
-export const toCats = () => "/zwierzeta/cats";
+export const toCats = () => "/zwierzeta/kotki";
 
 export const toArticle = ({ id } = { id: ":id" }) => `/news/${id}`;
