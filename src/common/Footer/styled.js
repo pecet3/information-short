7f6a2ecaf36992bf7ca15c6097bfe2b9e9287a96
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-14 11:09:16 +0100

    padding, and color in Content; backgroud-color and margin in Wrapper

--- src/common/Footer/styled.js
+++ src/common/Footer/styled.js
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.footer`
-    margin: 24px auto;
+    margin: 10px auto;
     font-size: 14px;
+    background-color: ${({ theme }) => theme.elements.headerBackground};
 `;
 
 export const Content = styled.p`
-    
+    padding: 6px;
+    color: ${({ theme }) => theme.elements.text};
 `;
