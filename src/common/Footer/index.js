6e0f7d41c50c91f7c2e40a6d588e34ecad106b6e
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-14 11:03:40 +0100

    created Footer

---
+++ src/common/Footer/index.js
@@ -0,0 +1,11 @@
+import { Content, Wrapper } from "./styled"
+
+export const Footer = () => {
+    return (
+        <Wrapper>
+            <Content>
+                (c) Jakub Pacewicz 2023
+            </Content>
+        </Wrapper>
+    )
+}
