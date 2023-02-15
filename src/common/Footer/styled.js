1016b8e33c2cdae9f056d4555e5518263f518134
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-15 22:39:05 +0100

    changed margin, added font-family, renamed colors

--- src/common/Footer/styled.js
+++ src/common/Footer/styled.js
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.footer`
-    margin: 10px auto;
+    margin: 32px auto 12px auto;
     font-size: 14px;
-    background-color: ${({ theme }) => theme.elements.headerBackground};
+    background-color: ${({ theme }) => theme.elements.primaryBackground};
 `;
 
 export const Content = styled.p`
-    padding: 6px;
-    color: ${({ theme }) => theme.elements.text};
+    font-family: 'Roboto Mono', monospace;
+    padding: 10px;
+    color: ${({ theme }) => theme.elements.primary};
 `;
