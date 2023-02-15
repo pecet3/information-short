3a5ef44964a50c1e3dd323627e00259d2a47ca9c
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-15 18:45:18 +0100

    removed oneColumn attribute - MainWrapper default has oneColumn, added threeColumns attribute, changed padding

--- src/common/MainWrapper.js
+++ src/common/MainWrapper.js
@@ -3,22 +3,27 @@ import styled, { css } from "styled-components";
 export const MainWrapper = styled.main`
     background-color: ${({ theme }) => theme.elements.bigTile};
     text-align: left;
-    padding: 10px;
+    padding: 0 10%;
     margin-top: 10px;
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-gap: 10px;
     color: ${({ theme }) => theme.elements.text};
-
-    ${({ oneColumn }) => oneColumn && css`
-        grid-template-columns: auto;
-    `}
+    display: grid;
 
     @media (max-width:${({ theme }) => theme.breakpoints.large}px){
+            padding: 10px;
+    }
+
+    ${({ threeColumns }) => threeColumns && css`
+        grid-template-columns: 1fr 1fr 1fr;
+        grid-gap: 10px;
+        padding: 10px;
+
+        @media (max-width:${({ theme }) => theme.breakpoints.large}px){
         grid-template-columns: 1fr 1fr;
-    }
-    @media (max-width:${({ theme }) => theme.breakpoints.mobile}px){
-        grid-template-columns: auto;
-    }
+        }
+
+        @media (max-width:${({ theme }) => theme.breakpoints.mobile}px){
+            grid-template-columns: auto;
+        }
+    `}
 
 `;
