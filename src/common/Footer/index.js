cd987a5a6b4497389ddb1d23c12dff4cfeeb4f57
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-16 20:39:37 +0100

    content as "a", href to github profile

--- src/common/Footer/index.js
+++ src/common/Footer/index.js
@@ -3,7 +3,9 @@ import { Content, Wrapper } from "./styled"
 export const Footer = () => {
     return (
         <Wrapper>
-            <Content>
+            <Content as="a"
+                href="https://github.com/pecet3"
+                title="link do mojego githuba">
                 Jakub Pacewicz 2023
             </Content>
         </Wrapper>
