546526d668baf3fa156c2d9b189c60637fedf07f
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-16 20:39:00 +0100

    added weatherSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -2,11 +2,13 @@ import { all } from "@redux-saga/core/effects";
 import { catsSaga } from "./features/Cats/catsSaga";
 import { dogsSaga } from "./features/Dogs/dogsSaga";
 import { newsSaga } from "./features/News/newsSaga";
+import { weatherSaga } from "./features/Weather/weatherSaga";
 
 export default function* rootSaga() {
     yield all([
         newsSaga(),
         dogsSaga(),
         catsSaga(),
+        weatherSaga(),
     ]);
 };
