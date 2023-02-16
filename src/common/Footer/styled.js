fea2878e9cc735b3ce057b536fdb1eda795f29aa
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-16 20:39:56 +0100

    styled visted pseudoclass

--- src/common/Footer/styled.js
+++ src/common/Footer/styled.js
@@ -2,17 +2,24 @@ import styled from "styled-components";
 
 export const Wrapper = styled.footer`
     margin: 32px auto 12px auto;
+    padding: 8px;
     font-size: 14px;
     background-color: ${({ theme }) => theme.elements.primaryBackground};
 `;
 
-export const Content = styled.p`
+export const Content = styled.a`
     font-family: 'Roboto Mono', monospace;
     padding: 10px;
     color: ${({ theme }) => theme.elements.primary};
+    text-decoration: none;
 
+    &:visited{
+        color: ${({ theme }) => theme.elements.primary};
+    }
     &::before{
         content: "// ";
         color: ${({ theme }) => theme.elements.primary + 70};
     }
+
+
 `;
