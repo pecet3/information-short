f765c30cc6504149d828115f181840ccad0848db
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-16 23:14:22 +0100

    crreated simple list with fetched data

--- src/features/Weather/index.js
+++ src/features/Weather/index.js
@@ -1,10 +1,42 @@
+import { nanoid } from "@reduxjs/toolkit";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { MainWrapper } from "../../common/MainWrapper";
+import { Tile } from "../News/Tile";
+import { fetchWeather, selectWeather, selectWeatherStatus } from "./weatherSlice";
 
 export const Weather = () => {
+    const dispatch = useDispatch();
+    const weather = useSelector(selectWeather);
+    const status = useSelector(selectWeatherStatus);
+
+
+    useEffect(() => {
+        dispatch(fetchWeather())
+    }, [dispatch])
+
+    useEffect(() => {
+        status === "success" &&
+            console.log(weather);
+    }, [weather, status]);
+
     return (
         <>
-            <MainWrapper>
-                <h2>Sekcja Pogody jest jeszcze w budowie</h2>
+            <MainWrapper threeColumns={true}>
+                <ul>
+                    {status === "success" &&
+                        weather.time.map((data) =>
+                            <li key={nanoid()}>
+                                {data}
+                            </li>)}
+                </ul>
+                <ul>
+                    {status === "success" &&
+                        weather.temperature_2m.map((data) =>
+                            <li key={nanoid()}>
+                                {data}
+                            </li>)}
+                </ul>
             </MainWrapper>
 
         </>
