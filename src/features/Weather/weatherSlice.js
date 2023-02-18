eebe3233ab214e23e775d851a524c40ce3c946b8
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-19 00:14:55 +0100

    created new reducer

--- src/features/Weather/weatherSlice.js
+++ src/features/Weather/weatherSlice.js
@@ -17,19 +17,32 @@ const weatherSlice = createSlice({
         fetchWeatherError: (state) => {
             state.status = "error"
         },
+        createTempTimeArray: (state) => {
+            state.tempTime = []
+
+            for (const i in state.data.hourly.time) {
+                state.tempTime.push
+                    ({
+                        time: state.data.hourly.time[i],
+                        temperature: state.data.hourly.temperature_2m[i]
+                    })
+            }
+
+            console.log(state.tempTime)
+
+        },
     },
 });
 
 export const {
     fetchWeather,
     fetchWeatherSuccess,
-    fetchWeatherError, } = weatherSlice.actions;
+    fetchWeatherError,
+    createTempTimeArray } = weatherSlice.actions;
 
 export const selectWeatherState = state => state.weather
 export const selectWeather = state => selectWeatherState(state).data;
 export const selectWeatherHourly = state => selectWeatherState(state).data.hourly;
-export const selectWeatherTemperature = state => selectWeatherHourly(state);
-export const selectWeatherTime = state => selectWeatherHourly(state);
 export const selectWeatherStatus = state => state.weather.status;
 
 
