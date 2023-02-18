dd8c7badf3555f4985208a2e8841147ce2321329
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-19 00:26:38 +0100

    Table insted list

--- src/features/Weather/index.js
+++ src/features/Weather/index.js
@@ -2,16 +2,15 @@ import { nanoid } from "@reduxjs/toolkit";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { MainWrapper } from "../../common/MainWrapper";
+import { Table, Td } from "./styled";
 
-import { fetchWeather, selectWeather, selectWeatherHourly, selectWeatherStatus, selectWeatherTemperature, selectWeatherTime } from "./weatherSlice";
+import { fetchWeather, selectTempTime, selectWeather, selectWeatherHourly, selectWeatherStatus, } from "./weatherSlice";
 
 export const Weather = () => {
     const dispatch = useDispatch();
     const weather = useSelector(selectWeather);
     const status = useSelector(selectWeatherStatus);
-    const temperature = useSelector(selectWeatherTemperature);
-    const time = useSelector(selectWeatherTime);
-    const hourly = useSelector(selectWeatherHourly);
+    const tempTime = useSelector(selectTempTime);
 
     useEffect(() => {
         status === "initial"
@@ -26,20 +25,22 @@ export const Weather = () => {
     return (
         <>
             <MainWrapper threeColumns={true}>
-                <ul>
-                    {status === "success" &&
-                        hourly.temperature_2m.map((data) =>
-                            <li key={nanoid()}>
-                                {data}
-                            </li>)}
-                </ul>
-                <ul>
-                    {status === "success" &&
-                        hourly.time.map((data) =>
-                            <li key={nanoid()}>
-                                {data}
-                            </li>)}
-                </ul>
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>Data</th>
+                            <th>Temperatura</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {status === "success" &&
+                            tempTime.map((element) =>
+                                <tr key={nanoid()}>
+                                    <Td>{element.time}</Td>
+                                    <Td>{element.temperature}</Td>
+                                </tr>)}
+                    </tbody>
+                </Table>
             </MainWrapper>
 
         </>
