3246a43337b3b123356f33b104bb6d4e857dd395
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-19 23:50:23 +0100

    updated cities

--- src/features/Weather/cities.js
+++ src/features/Weather/cities.js
@@ -1,52 +1,98 @@
-export const cities = [
+const cities = [
     {
-        name: "Warszawa",
-        latitude: 52.2298,
-        longitude: 21.0118,
-    },
-    {
-        name: "Kraków",
-        latitude: 50.0647,
-        longitude: 19.9450,
-    },
-    {
-        name: "Łódź",
-        latitude: 51.7592,
-        longitude: 19.4558,
-    },
-    {
-        name: "Wrocław",
-        latitude: 51.1079,
-        longitude: 17.0385,
-    },
-    {
-        name: "Poznań",
-        latitude: 52.4080,
-        longitude: 16.9335,
-    },
-    {
-        name: "Gdańsk",
-        latitude: 54.3722,
-        longitude: 18.6383,
-    },
-    {
-        name: "Szczecin",
-        latitude: 53.4289,
-        longitude: 14.5530,
+        name: "Białystok",
+        latitude: 53.1325,
+        longitude: 23.1688,
+        voivodeship: "podlaskie"
     },
     {
         name: "Bydgoszcz",
         latitude: 53.1235,
         longitude: 18.0084,
+        voivodeship: "kujawsko-pomorskie"
+    },
+    {
+        name: "Gdańsk",
+        latitude: 54.3722,
+        longitude: 18.6383,
+        voivodeship: "pomorskie"
+    },
+    {
+        name: "Gorzów Wielkopolski",
+        latitude: 52.7361,
+        longitude: 15.2282,
+        voivodeship: "lubuskie"
+    },
+    {
+        name: "Kielce",
+        latitude: 50.8661,
+        longitude: 20.6286,
+        voivodeship: "świętokrzyskie"
+    },
+    {
+        name: "Kraków",
+        latitude: 50.0647,
+        longitude: 19.9450,
+        voivodeship: "małopolskie"
     },
     {
         name: "Lublin",
         latitude: 51.2465,
         longitude: 22.5684,
+        voivodeship: "lubelskie"
     },
     {
-        name: "Katowice",
-        latitude: 50.2649,
-        longitude: 19.0238,
+        name: "Łódź",
+        latitude: 51.7592,
+        longitude: 19.4558,
+        voivodeship: "łódzkie"
     },
-];
+    {
+        name: "Olsztyn",
+        latitude: 53.7784,
+        longitude: 20.4801,
+        voivodeship: "warmińsko-mazurskie"
+    },
+    {
+        name: "Opole",
+        latitude: 50.6745,
+        longitude: 17.9269,
+        voivodeship: "opolskie"
+    },
+    {
+        name: "Poznań",
+        latitude: 52.4080,
+        longitude: 16.9335,
+        voivodeship: "wielkopolskie"
+    },
+    {
+        name: "Rzeszów",
+        latitude: 50.0412,
+        longitude: 21.9991,
+        voivodeship: "podkarpackie"
+    },
+    {
+        name: "Szczecin",
+        latitude: 53.4289,
+        longitude: 14.5530,
+        voivodeship: "zachodniopomorskie"
+    },
+    {
+        name: "Toruń",
+        latitude: 53.0138,
+        longitude: 18.5981,
+        voivodeship: "kujawsko-pomorskie"
+    },
+    {
+        name: "Warszawa",
+        latitude: 52.2298,
+        longitude: 21.0118,
+        voivodeship: "mazowieckie"
+    },
+    {
+        name: "Wrocław",
+        latitude: 51.1079,
+        longitude: 17.0385,
+        voivodeship: "dolnośląskie"
+    },
+]
