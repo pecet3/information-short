73e9588c00545037c747479e44b22c596a2154e5
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-19 23:50:42 +0100

    link to fetch as function

--- src/features/Weather/getWeather.js
+++ src/features/Weather/getWeather.js
@@ -1,7 +1,11 @@
-const link = "https://api.open-meteo.com/v1/forecast?latitude=54.35&longitude=18.65&hourly=temperature_2m";
+const getLink = () => {
+    const link = "https://api.open-meteo.com/v1/forecast?latitude=54.35&longitude=18.65&hourly=temperature_2m";
+    return link
+}
+
 
 export const getWeather = async () => {
-    const response = await fetch(link);
+    const response = await fetch(getLink());
 
     if (!response.ok) {
         throw new Error(response.message)
