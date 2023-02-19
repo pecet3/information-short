bfd0bcfab85dd87608917268bf1f340e6ae02e21
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-19 23:39:14 +0100

    created the file

---
+++ src/features/Weather/cities.js
@@ -0,0 +1,52 @@
+export const cities = [
+    {
+        name: "Warszawa",
+        latitude: 52.2298,
+        longitude: 21.0118,
+    },
+    {
+        name: "Kraków",
+        latitude: 50.0647,
+        longitude: 19.9450,
+    },
+    {
+        name: "Łódź",
+        latitude: 51.7592,
+        longitude: 19.4558,
+    },
+    {
+        name: "Wrocław",
+        latitude: 51.1079,
+        longitude: 17.0385,
+    },
+    {
+        name: "Poznań",
+        latitude: 52.4080,
+        longitude: 16.9335,
+    },
+    {
+        name: "Gdańsk",
+        latitude: 54.3722,
+        longitude: 18.6383,
+    },
+    {
+        name: "Szczecin",
+        latitude: 53.4289,
+        longitude: 14.5530,
+    },
+    {
+        name: "Bydgoszcz",
+        latitude: 53.1235,
+        longitude: 18.0084,
+    },
+    {
+        name: "Lublin",
+        latitude: 51.2465,
+        longitude: 22.5684,
+    },
+    {
+        name: "Katowice",
+        latitude: 50.2649,
+        longitude: 19.0238,
+    },
+];
