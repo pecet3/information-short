6c48236a81d009c986fe45dd2412e8e1eef9a5db
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-19 12:40:08 +0100

    added text-align: center and margin: 0 auto in Table

--- src/features/Weather/styled.js
+++ src/features/Weather/styled.js
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const Table = styled.table`
     border: 1px solid;
     border-collapse: collapse;
-
+    text-align: center;
+    margin: 0 auto;
 `
 export const Td = styled.td`
     border: 1px solid;
