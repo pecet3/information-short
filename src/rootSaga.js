52716891564e9a14268448a944b56214edd0d214
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-21 19:43:36 +0100

    deleted weatherSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -2,13 +2,11 @@ import { all } from "@redux-saga/core/effects";
 import { catsSaga } from "./features/Cats/catsSaga";
 import { dogsSaga } from "./features/Dogs/dogsSaga";
 import { newsSaga } from "./features/News/newsSaga";
-import { weatherSaga } from "./features/Weather/weatherSaga";
 
 export default function* rootSaga() {
     yield all([
         newsSaga(),
         dogsSaga(),
         catsSaga(),
-        weatherSaga(),
     ]);
 };
