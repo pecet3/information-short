b001d97164855d2920d25817b505e6b2a713e9ee
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-21 20:09:01 +0100

    fetch in useEffect

--- src/features/Weather/index.js
+++ src/features/Weather/index.js
@@ -1,24 +1,38 @@
 import { useEffect } from "react";
+import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { MainWrapper } from "../../common/MainWrapper";
 import {
+    createTempTimeArray,
     fetchWeather,
-    selectWeatherStatus,
+    fetchWeatherSuccess,
+    selectCityIndex,
 } from "./weatherSlice";
 import { Chart } from "./Chart";
 import { Form } from "./Form";
 import { Table } from "./Table";
+import { cities } from "./cities";
 
 
 export const Weather = () => {
     const dispatch = useDispatch();
-    const status = useSelector(selectWeatherStatus);
+    const cityIndex = useSelector(selectCityIndex);
 
     useEffect(() => {
-        status === "initial"
-            && dispatch(fetchWeather())
-    }, [dispatch, status])
-
+        const getWeather = async () => {
+            dispatch(fetchWeather());
+            try {
+                const response = await axios.get(
+                    `https://api.open-meteo.com/v1/forecast?latitude=${cities[cityIndex].latitude}&longitude=${cities[cityIndex].longitude}&hourly=temperature_2m`
+                );
+                dispatch(fetchWeatherSuccess(response.data));
+                await dispatch(createTempTimeArray());
+            } catch (error) {
+                dispatch(fetchWeatherSuccess());
+            }
+        };
+        getWeather();
+    }, [cityIndex]);
 
 
     return (
