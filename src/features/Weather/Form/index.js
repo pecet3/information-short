7d9c73968654dc8623e71d8096fdb87b9aaa8ec6
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-21 18:10:18 +0100

    removed useState with cityName, value in select is state from slice

--- src/features/Weather/Form/index.js
+++ src/features/Weather/Form/index.js
@@ -1,20 +1,18 @@
-import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectCityIndex, setCityIndex } from "../weatherSlice";
 import { cities } from "./cities";
 export const Form = () => {
-    const [cityName, setCityName] = useState(0)
+
+    const dispatch = useDispatch();
+    const cityIndex = useSelector(selectCityIndex);
 
     const onSelectChange = ({ target }) => (
-        setCityName(target.value)
-    )
-
-    useEffect(() => {
-        console.log(cities[cityName])
-    }, [cityName])
-
+        dispatch(setCityIndex(target.value))
+    );
 
     return (
         <form onSubmit={(event) => event.preventDefault()}>
-            <select name="weather" value={cityName} onChange={onSelectChange}>
+            <select name="weather" value={cityIndex} onChange={onSelectChange}>
                 {cities.map((city) =>
                     <option
                         key={city.name}
