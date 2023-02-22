eb4d718847592d5d19c8d5fcb27eae5eb46d42ac
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-22 21:22:17 +0100

    created array

---
+++ src/features/Weather/Form/daysToDisplay.js
@@ -0,0 +1,9 @@
+export const daysToDisplay = [
+    { name: '1 dzień', value: 24 },
+    { name: '2 dni', value: 48 },
+    { name: '3 dni', value: 72 },
+    { name: '4 dni', value: 96 },
+    { name: '5 dni', value: 120 },
+    { name: '6 dni', value: 144 },
+    { name: '7 dni', value: 168 }
+]
