a9a97b0507cf25190f86281d6e530a7ea1a87bc8
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-22 22:50:59 +0100

    styled Select, Wrapper and StyledForm

--- src/features/Weather/Form/styled.js
+++ src/features/Weather/Form/styled.js
@@ -1,14 +1,22 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 export const Wrapper = styled.div`
+    margin: 10px 0;
+    padding: 0 8%;
 `;
 export const StyledForm = styled.form`
-    
+    display: flex;
+    gap: 10%;
+
+    @media(max-width:${({ theme }) => theme.breakpoints.mobile}px){
+        flex-direction: column;
+    }
 `;
 export const Legend = styled.legend`
 
 `;
 export const Select = styled.select`
+    width: 160px;
 `;
 export const Option = styled.option`
 
