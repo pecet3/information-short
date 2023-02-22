a4364e13d229f1082bd53c2bbedb7a803480dd6d
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-22 20:58:57 +0100

    created styled compontents

---
+++ src/features/Weather/Form/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledForm = styled.form`
+    
+`;
+export const Legend = styled.legend`
+
+`;
+export const Select = styled.select`
+`;
+export const Option = styled.option`
+
+`;
