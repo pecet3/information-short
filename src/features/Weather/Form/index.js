06e4169b86d7974b35ca61f72a2d154eb6c6df8c
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-22 21:04:10 +0100

    added Wrapper, renamed onSelectChange to onCitiesChange, created onDaysChange

--- src/features/Weather/Form/index.js
+++ src/features/Weather/Form/index.js
@@ -1,26 +1,34 @@
 import { useDispatch, useSelector } from "react-redux";
 import { selectCityIndex, setCityIndex } from "../weatherSlice";
 import { cities } from "../cities";
-import { StyledForm, Legend, Select, Option } from "./styled";
+import { StyledForm, Legend, Select, Option, Wrapper } from "./styled";
 export const Form = () => {
     const dispatch = useDispatch();
     const cityIndex = useSelector(selectCityIndex);
 
-    const onSelectChange = ({ target }) => (
+    const onCitiesChange = ({ target }) => (
         dispatch(setCityIndex(target.value))
+    );
+    const onDaysChange = ({ target }) => (
+        console.log(target.value)
     );
 
     return (
-        <StyledForm onSubmit={(event) => event.preventDefault()}>
-            <Legend>Wybierz miasto</Legend>
-            <Select name="weather" value={cityIndex} onChange={onSelectChange}>
-                {cities.map((city) =>
-                    <Option
-                        key={city.name}
-                        value={cities.indexOf(city)}>
-                        {city.name}
-                    </Option>)}
-            </Select>
-        </StyledForm>
+        <Wrapper>
+            <StyledForm onSubmit={(event) => event.preventDefault()}>
+                <Legend>Wybierz miasto</Legend>
+                <Select name="weather" value={cityIndex} onChange={onCitiesChange}>
+                    {cities.map((city) =>
+                        <Option
+                            key={city.name}
+                            value={cities.indexOf(city)}>
+                            {city.name}
+                        </Option>)}
+                </Select>
+                <Select name="days" onChange={onDaysChange}>
+
+                </Select>
+            </StyledForm>
+        </Wrapper>
     )
 }
