06414f1654205255013166b723266ec1610a3cb1
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-22 22:50:40 +0100

    added spans

--- src/features/Weather/Form/index.js
+++ src/features/Weather/Form/index.js
@@ -1,8 +1,8 @@
 import { useDispatch, useSelector } from "react-redux";
-import { selectCityIndex, setCityIndex, setDaysToDisplay } from "../weatherSlice";
+import { selectCityIndex, setCityIndex, setHoursToDisplay } from "../weatherSlice";
 import { cities } from "../cities";
 import { StyledForm, Legend, Select, Option, Wrapper } from "./styled";
-import { daysToDisplay } from "./daysToDisplay";
+import { getHoursToDisplay } from "./getHoursToDisplay";
 export const Form = () => {
     const dispatch = useDispatch();
     const cityIndex = useSelector(selectCityIndex);
@@ -11,30 +11,35 @@ export const Form = () => {
         dispatch(setCityIndex(target.value))
     );
     const onDaysChange = ({ target }) => (
-        dispatch(setDaysToDisplay(target.value))
+        dispatch(setHoursToDisplay(target.value))
     );
 
     return (
         <Wrapper>
             <StyledForm onSubmit={(event) => event.preventDefault()}>
-                <Legend>Wybierz miasto</Legend>
-                <Select name="weather" value={cityIndex} onChange={onCitiesChange}>
-                    {cities.map(city =>
-                        <Option
-                            key={city.name}
-                            value={cities.indexOf(city)}>
-                            {city.name}
-                        </Option>)}
-                </Select>
-                <Select name="days" onChange={onDaysChange}>
-                    {daysToDisplay().map(day =>
-                        <Option
-                            key={day.name}
-                            value={day.value}>
-                            {day.name}
-                        </Option>
-                    )}
-                </Select>
+                <span>
+                    <Legend>Wybierz miasto</Legend>
+                    <Select name="weather" value={cityIndex} onChange={onCitiesChange}>
+                        {cities.map(city =>
+                            <Option
+                                key={city.name}
+                                value={cities.indexOf(city)}>
+                                {city.name}
+                            </Option>)}
+                    </Select>
+                </span>
+                <span>
+                    <Legend>Wybierz liczbę dni</Legend>
+                    <Select name="days" onChange={onDaysChange}>
+                        {getHoursToDisplay().map(day =>
+                            <Option
+                                key={day.name}
+                                value={day.value}>
+                                {day.name}
+                            </Option>
+                        )}
+                    </Select>
+                </span>
             </StyledForm>
         </Wrapper>
     )
