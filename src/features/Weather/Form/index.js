67dafa289b43520413cd064d911ae89cb0c0688c
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-23 22:09:59 +0100

    added Button

--- src/features/Weather/Form/index.js
+++ src/features/Weather/Form/index.js
@@ -1,11 +1,12 @@
 import { useDispatch, useSelector } from "react-redux";
-import { selectCityIndex, setCityIndex, setHoursToDisplay } from "../weatherSlice";
+import { selectCityIndex, selectShowTable, setCityIndex, setHoursToDisplay, setShowTable } from "../weatherSlice";
 import { cities } from "../cities";
-import { StyledForm, Legend, Select, Option, Wrapper, Span } from "./styled";
+import { StyledForm, Legend, Select, Option, Wrapper, Span, Button } from "./styled";
 import { getHoursToDisplay } from "./getHoursToDisplay";
 export const Form = () => {
     const dispatch = useDispatch();
     const cityIndex = useSelector(selectCityIndex);
+    const showTable = useSelector(selectShowTable);
 
     const onCitiesChange = ({ target }) => (
         dispatch(setCityIndex(target.value))
@@ -40,6 +41,9 @@ export const Form = () => {
                         )}
                     </Select>
                 </Span>
+                <Button onClick={() => dispatch(setShowTable())}>
+                    {showTable ? "Ukryj" : "Pokaż"} Tabelę
+                </Button>
             </StyledForm>
         </Wrapper>
     )
