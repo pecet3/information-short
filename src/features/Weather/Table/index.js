484245d966316fbc4ea7132a453e3d824fc232e2
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-27 21:37:45 +0100

    added Wrapper

--- src/features/Weather/Table/index.js
+++ src/features/Weather/Table/index.js
@@ -1,7 +1,7 @@
 import { nanoid } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
 import { selectHoursToDisplay, selectShowTable, selectdataArray, selectWeather, selectWeatherStatus } from "../weatherSlice";
-import { StyledTable, Td, Th, Tr } from "./styled";
+import { StyledTable, Td, Th, Tr, Wrapper } from "./styled";
 export const Table = () => {
     const weather = useSelector(selectWeather)
     const dataArray = useSelector(selectdataArray);
@@ -9,41 +9,43 @@ export const Table = () => {
     const hoursToDisplay = useSelector(selectHoursToDisplay);
     const showTable = useSelector(selectShowTable);
     return (
-        <StyledTable noRender={!showTable ? true : false}>
-            <thead>
-                <tr>
-                    <Th as="th">Data</Th>
-                    <Th as="th">Godzina</Th>
-                    <Th as="th">Temperatura</Th>
-                    <Th as="th">
-                        Odczuwalna
-                        <br />Temperatura
-                    </Th>
-                </tr>
-            </thead>
-            <tbody>
-                {status === "success" &&
-                    dataArray.slice(0, hoursToDisplay).map((element) =>
-                        <Tr key={nanoid()}>
-                            <Td scope="row">
-                                {element.date.toLocaleDateString(undefined, {
-                                    weekday: "long",
-                                    day: "numeric",
-                                    month: "long",
-                                    year: "numeric",
-                                })}
-                            </Td>
-                            <Td scope="row">
-                                {element.date.toLocaleTimeString('pl-PL')}
-                            </Td>
-                            <Td scope="row">
-                                {element.temperature} {weather.hourly_units.temperature_2m}
-                            </Td>
-                            <Td scope="row">
-                                {element.apparentTemperature} {weather.hourly_units.temperature_2m}
-                            </Td>
-                        </Tr>)}
-            </tbody>
-        </StyledTable>
+        <Wrapper>
+            <StyledTable noRender={!showTable ? true : false}>
+                <thead>
+                    <tr>
+                        <Th as="th">Data</Th>
+                        <Th as="th">Godzina</Th>
+                        <Th as="th">Temperatura</Th>
+                        <Th as="th">
+                            Odczuwalna
+                            <br />Temperatura
+                        </Th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {status === "success" &&
+                        dataArray.slice(0, hoursToDisplay).map((element) =>
+                            <Tr key={nanoid()}>
+                                <Td scope="row">
+                                    {element.date.toLocaleDateString(undefined, {
+                                        weekday: "long",
+                                        day: "numeric",
+                                        month: "long",
+                                        year: "numeric",
+                                    })}
+                                </Td>
+                                <Td scope="row">
+                                    {element.date.toLocaleTimeString('pl-PL')}
+                                </Td>
+                                <Td scope="row">
+                                    {element.temperature} {weather.hourly_units.temperature_2m}
+                                </Td>
+                                <Td scope="row">
+                                    {element.apparentTemperature} {weather.hourly_units.temperature_2m}
+                                </Td>
+                            </Tr>)}
+                </tbody>
+            </StyledTable>
+        </Wrapper>
     )
 }
