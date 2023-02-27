f09d4cfc7ac81ae9daa669a6b02f3836d024058a
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-27 20:21:26 +0100

    styled all components

--- src/features/Weather/charts/styled.js
+++ src/features/Weather/charts/styled.js
@@ -2,8 +2,9 @@ import { Line } from "react-chartjs-2";
 import styled from "styled-components";
 
 export const StyledLine = styled(Line)`
-    
+    width: 200%;
+    height: 200%;
 `;
 export const Container = styled.div`
-
+    max-width: 100%;
 `;
