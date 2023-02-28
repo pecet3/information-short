14db2a9f6a73f6b820e1f95fd3b852f3a8c66ac6
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-28 16:09:06 +0100

    added StyledLink, styled other componentes

--- src/common/Navigation/MobileNavigation/styled.js
+++ src/common/Navigation/MobileNavigation/styled.js
@@ -1,19 +1,21 @@
 import styled, { css } from "styled-components";
 import { ReactComponent as ListSvg } from "./list.svg";
 import { ReactComponent as CrossSvg } from "./cross.svg";
+import { NavLink } from "react-router-dom";
 
 export const Wrapper = styled.div`
+    background-color: ${({ theme }) => theme.elements.navigationBackground};
     display: flex;
     flex-direction: column;
     width: 100%;
 `;
 export const ListIcon = styled(ListSvg)`
-    width: 28px;
-    height: auto;
+    width: 32px;
+    height: 32px;
 `;
 export const CrossIcon = styled(CrossSvg)`
-    width: 28px;
-    height: auto;
+    width: 32px;
+    height: 32px;
 `;
 export const List = styled.ul`
     display: none;
@@ -21,7 +23,8 @@ export const List = styled.ul`
     background-color: ${({ theme }) => theme.elements.navigationMobile};
     align-self: center;
     margin: 0;
-    padding: 0 100%;
+    padding: 6px 100%;
+    text-align: left;
 
     ${({ display }) => display && css`
         display: flex;
@@ -31,7 +34,30 @@ export const List = styled.ul`
 export const Button = styled.button`
     display: none;
     align-self: flex-end;
+    padding: 0;
+    margin: 0 6px 0 0;
+    border: none;
+    background-color: transparent;
+
     @media (max-width:${({ theme }) => theme.breakpoints.mobile}px){
         display: block;
     }
 `;
+
+export const StyledNavLink = styled(NavLink)`
+    text-decoration: none;
+    font-weight: 700;
+    letter-spacing: 0.1em;
+    color: ${({ theme }) => theme.elements.navigationText};
+    transition: color 0.4s;
+    padding: 0;
+
+    &:visited{
+        text-decoration: none;
+        color: ${({ theme }) => theme.elements.navigationText};
+        
+        &.active{
+        color: ${({ theme }) => theme.elements.primary};
+        }
+    }
+`
