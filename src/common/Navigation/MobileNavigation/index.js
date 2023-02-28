f6ed7e2f7de58a589c5ba2bc0f5b062a675ec707
Jakub Pacewicz <pecet3107@gmail.coim>
2023-02-28 15:29:30 +0100

    created component

---
+++ src/common/Navigation/MobileNavigation/index.js
@@ -0,0 +1,24 @@
+import { useState } from "react"
+import { NavLink } from "react-router-dom"
+import { toNews, toWeather } from "../../../routes"
+import { Wrapper, ListIcon, List, Button, CrossIcon } from "./styled"
+
+export const MobileNavigation = () => {
+    const [showList, setShowList] = useState(false);
+
+    return (
+        <Wrapper>
+            <Button onClick={() => setShowList(!showList)}>
+                {showList ? <ListIcon /> : <CrossIcon />}
+            </Button>
+            <List display={showList}>
+                <li>
+                    <NavLink to={toNews()}>News</NavLink>
+                </li>
+                <li>
+                    <NavLink to={toWeather()}>Pogoda</NavLink>
+                </li>
+            </List>
+        </Wrapper>
+    )
+}
