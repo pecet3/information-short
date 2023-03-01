77a7b1a505baff9b97869e9d3a5ec3d26573ad82
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-01 10:00:34 +0100

    replaced useCurrentDate to the separate file

---
+++ src/common/Header/useCurrentDate.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+export const useCurrentDate = () => {
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date());
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    return date;
+};
