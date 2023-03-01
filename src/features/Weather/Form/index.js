212fc8f3d4bcac4f39bb8e1b077c513ea71811e5
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-01 12:52:36 +0100

    removed showTable, edited onShowDataChange, removed Button

--- src/features/Weather/Form/index.js
+++ src/features/Weather/Form/index.js
@@ -2,12 +2,10 @@ import { useDispatch, useSelector } from "react-redux";
 import {
     selectCityIndex,
     selectShowData,
-    selectShowTable,
     setCityIndex,
     setHoursToDisplay,
     setShowApparentTemperature,
     setShowTable,
-    setShowTemperature
 } from "../weatherSlice";
 import { cities } from "../cities";
 import { StyledForm, Legend, Select, Option, Wrapper, Span, Button, Label, Input } from "./styled";
@@ -16,7 +14,6 @@ import { nanoid } from "@reduxjs/toolkit";
 export const Form = () => {
     const dispatch = useDispatch();
     const cityIndex = useSelector(selectCityIndex);
-    const showTable = useSelector(selectShowTable);
     const showData = useSelector(selectShowData);
 
     const onCitiesChange = ({ target }) => (
@@ -26,8 +23,8 @@ export const Form = () => {
         dispatch(setHoursToDisplay(target.value))
     );
     const onShowDataChange = ({ target }) => {
-        target.name === "temperature"
-            && dispatch(setShowTemperature())
+        target.name === "table"
+            && dispatch(setShowTable())
         target.name === "apparent temperature"
             && dispatch(setShowApparentTemperature());
     };
@@ -57,11 +54,6 @@ export const Form = () => {
                             </Option>
                         )}
                     </Select>
-                </Span>
-                <Span>
-                    <Button onClick={() => dispatch(setShowTable())}>
-                        {showTable ? "Ukryj" : "Pokaż"} Tabelę
-                    </Button>
                 </Span>
                 {showData.map(element =>
                     <Span key={nanoid()}>
