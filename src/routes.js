0e05f9a70a3fd7496d7d2c7b91ccf73722a98692
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-02 23:23:54 +0100

    added toCurrencies

--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,6 @@ export const toWeather = () => "/pogoda";
 export const toPets = () => "/zwierzeta";
 export const toDogs = () => "/zwierzeta/pieski";
 export const toCats = () => "/zwierzeta/kotki";
+export const toCurrencies = () => "/waluty";
 
 export const toArticle = ({ id } = { id: ":id" }) => `/news/${id}`;
