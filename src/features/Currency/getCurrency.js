50d1f1e710825a950a71281d98793b4ff7cbee05
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-03 10:45:53 +0100

    edited function

--- src/features/Currency/getCurrency.js
+++ src/features/Currency/getCurrency.js
@@ -5,7 +5,7 @@ export const getCurrency = async (dispatch) => {
     dispatch(fetchCurrency())
     try {
         const response = await axios.get("https://api.nbp.pl/api/exchangerates/tables/a/last/7?format=json")
-        dispatch(fetchCurrencySuccess(response.data))
+        await dispatch(fetchCurrencySuccess(response.data))
     } catch {
         dispatch(fetchCurrencyError())
     }
