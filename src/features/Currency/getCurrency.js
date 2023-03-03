851df6b3d0e5738eb4947967a779c29c19ea8020
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-03 10:23:53 +0100

    edited function

--- src/features/Currency/getCurrency.js
+++ src/features/Currency/getCurrency.js
@@ -1,6 +1,13 @@
 import axios from "axios";
+import { fetchCurrency, fetchCurrencySuccess, fetchWCurrencyError } from "./currencySlice";
 
-export const getCurrency = async () => {
-    const data = await axios.get("https://api.nbp.pl/api/exchangerates/tables/a/last/7?format=json")
+export const getCurrency = async (dispatch) => {
+    dispatch(fetchCurrency());
+    try {
+        const response = await axios.get("https://api.nbp.pl/api/exchangerates/tables/a/last/7?format=json");
+        await dispatch(fetchCurrencySuccess(response));
+    } catch (error) {
+        dispatch(fetchWCurrencyError());
+    }
 
 }; 
