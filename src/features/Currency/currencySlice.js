8f0f4e692aa27613a7fb5fc8dc91d64172f806c4
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-03 09:50:06 +0100

    created slice

---
+++ src/features/Currency/currencySlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const currencySlice = createSlice({
+    name: "currency",
+    initialState: {
+        data: [],
+        status: "initial",
+    },
+    reducers: {
+        fetchCurrency: (state) => {
+            state.status = "loading"
+        },
+        fetchCurrencySuccess: (state, { payload: currency }) => {
+            state.status = "success"
+            state.data = currency
+        },
+        fetchCurrencyError: (state) => {
+            state.status = "error"
+        },
+    },
+});
+
+export const {
+    fetchCurrency,
+    fetchCurrencySuccess,
+    fetchWCurrencyError, } = currencySlice.actions;
+
+export const selectCurrencyState = state => state.currency;
+export const selectCurrency = state => selectCurrencyState(state).data;
+export const selectCurrencyStatus = state => state.currency.status;
+
+export default currencySlice.reducer;   
