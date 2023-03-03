b6d0ca164a2ada84e1d31531767c1160b79f9a03
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-03 10:44:00 +0100

    added useEffect

--- src/features/Currency/index.js
+++ src/features/Currency/index.js
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { MainWrapper } from "../../common/MainWrapper";
+import { selectCurrencyStatus } from "./currencySlice";
+import { getCurrency } from "./getCurrency";
 
 export const Currency = () => {
+    const status = useSelector(selectCurrencyStatus);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        status === "initial"
+            && getCurrency(dispatch)
+    }, [dispatch, status])
+
     return (
         <MainWrapper>
             <p>test</p>
