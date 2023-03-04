0acf6d55c50ae4337eaff1d7b394b8fa69eeaaf5
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-04 19:53:55 +0100

    test select

--- src/features/Currency/Form/index.js
+++ src/features/Currency/Form/index.js
@@ -1,34 +1,27 @@
 import { nanoid } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux"
-import { selectCurrency, selectCurrencyList, selectCurrencyStatus } from "../currencySlice"
+import { selectCurrency, selectCurrencyStatus } from "../currencySlice"
 
 export const Form = () => {
     const status = useSelector(selectCurrencyStatus);
     const currency = useSelector(selectCurrency);
-    const currencyList = useSelector(selectCurrencyList);
     return (
-        <form onSubmit={(event) => event.preventDefault()}>
-            <label>
-                Wybierz walutę
-            </label>
-            <select>
-                {status === "success"
-                    && currencyList.rates.map(element =>
-                        <option
-                            key={nanoid()}
-                            value={element.currency}>
-                            {element.currency}
-                        </option>)}
-            </select>
-            <select>
-                {status === "success"
-                    && currency[0].rates.map(element =>
-                        <option
-                            key={nanoid()}
-                            value={element.currency}>
-                            {element.currency}
-                        </option>)}
-            </select>
-        </form>
+        <>
+            <form onSubmit={(event) => event.preventDefault()}>
+                <label>
+                    Wybierz walutę
+                </label>
+                <select>
+                    {status === "success"
+                        && currency[0].rates.map((element, index) =>
+                            <option
+                                key={nanoid()}
+                                value={element.currency}>
+                                {element.currency}
+                                {index}
+                            </option>)}
+                </select>
+            </form>
+        </>
     )
 }
