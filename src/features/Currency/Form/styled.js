e5e0f746ad94ec8206df67a2e5c3413d74e977c0
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-05 14:47:29 +0100

    created styled-compontents

---
+++ src/features/Currency/Form/styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StyledForm = styled.form`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const Label = styled.label`
+
+`;
+
+export const Select = styled.select`
+
+`;
+
+export const Option = styled.option`
+
+`;
