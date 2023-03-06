3c5dcfef1c27fbd9a98e58eab461be314661aa06
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-06 21:20:06 +0100

    created function handle, imported and implemented selector

--- src/features/Currency/Form/index.js
+++ src/features/Currency/Form/index.js
@@ -1,7 +1,14 @@
 import { nanoid } from "@reduxjs/toolkit";
 import { useDispatch, useSelector } from "react-redux"
-import { selectCurrency, selectCurrencyStatus, selectCurrencyIndex, setCurrencyIndex } from "../currencySlice"
-import { getDaysToDisplay } from "./getWeeksToDisplay";
+import {
+    selectCurrency,
+    selectCurrencyStatus,
+    selectCurrencyIndex,
+    setCurrencyIndex,
+    selectDaysToDisplay,
+    setDaysToDisplay
+} from "../currencySlice"
+import { getWeeks } from "./getWeeks";
 import { Label, Option, Select, SelectContainer, StyledForm } from "./styled";
 
 export const Form = () => {
@@ -9,30 +16,46 @@ export const Form = () => {
     const status = useSelector(selectCurrencyStatus);
     const currency = useSelector(selectCurrency);
     const currencyIndex = useSelector(selectCurrencyIndex);
+    const daysToDisplay = useSelector(selectDaysToDisplay);
 
     const onCurrencyIndexChange = ({ target }) => {
         dispatch(setCurrencyIndex(target.value));
     };
 
-    return (
-        <>
-            <StyledForm onSubmit={(event) => event.preventDefault()}>
-                <SelectContainer>
-                    <Label>
-                        Wybierz walutę
-                    </Label>
-                    <Select name="currencyIndex" value={currencyIndex} onChange={onCurrencyIndexChange}>
-                        {status === "success"
-                            && currency[0].rates.map((element, index) =>
-                                <Option
-                                    key={nanoid()}
-                                    value={index}>
-                                    {element.currency}
-                                </Option>)}
-                    </Select>
-                </SelectContainer>
+    const onDaysToDisplayChange = ({ target }) => {
+        dispatch(setDaysToDisplay(target.value));
+    };
 
-            </StyledForm>
-        </>
+    return (
+        <StyledForm onSubmit={(event) => event.preventDefault()}>
+            <SelectContainer>
+                <Label>
+                    Wybierz walutę
+                </Label>
+                <Select name="currencyIndex" value={currencyIndex} onChange={onCurrencyIndexChange}>
+                    {status === "success"
+                        && currency[0].rates.map((element, index) =>
+                            <Option
+                                key={nanoid()}
+                                value={index}>
+                                {element.currency}
+                            </Option>)}
+                </Select>
+            </SelectContainer>
+            <SelectContainer>
+                <Label>
+                    Podaj Liczbę tygodni
+                </Label>
+                <Select name="daysToDisplay" value={daysToDisplay} onChange={onDaysToDisplayChange}>
+                    {getWeeks().map(week =>
+                        <Option
+                            key={nanoid()}
+                            value={week.value}>
+                            {week.name}
+                        </Option>
+                    )}
+                </Select>
+            </SelectContainer>
+        </StyledForm>
     )
 }
