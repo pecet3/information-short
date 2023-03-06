1abf1bf33f4c11415b524ffdbbd80d6fd70f2b84
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-06 21:03:44 +0100

    created the function

---
+++ src/features/Currency/Form/getWeeksToDisplay.js
@@ -0,0 +1,19 @@
+export const getDaysToDisplay = () => {
+    const array = [];
+
+    for (let i = 1; i < 8; i++) {
+        let week = ""
+        if (i === 1) {
+            week = "1 tydzień"
+        } else if (i < 5) {
+            week = `${i} tygodnie`
+        } else {
+            week = `${i} tygodni`
+        }
+        array.push({
+            name: week,
+            value: i * 7,
+        });
+    };
+    return array;
+};
