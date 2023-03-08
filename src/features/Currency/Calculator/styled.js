e900460ba7db55efb442270a4607cc16f860ccd0
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-08 18:54:17 +0100

    created Wrapper, Form, Input and Result

---
+++ src/features/Currency/Calculator/styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+
+`
+
+export const Form = styled.form`
+
+`;
+
+export const Input = styled.input`
+
+`;
+
+export const Result = styled.span`
+
+`;
