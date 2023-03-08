91c6d5c5d51eefacfbcc8d1caa54be5ec38e7e47
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-08 18:57:15 +0100

    imported selectors

--- src/features/Currency/Calculator/index.js
+++ src/features/Currency/Calculator/index.js
@@ -1,11 +1,20 @@
+import { useSelector } from "react-redux"
+import { selectCurrency, selectCurrencyIndex, selectCurrencyStatus } from "../currencySlice"
 import { Form, Wrapper, Input } from "./styled"
 
 export const Calculator = () => {
+    const status = useSelector(selectCurrencyStatus);
+    const currency = useSelector(selectCurrency);
+    const currencyIndex = useSelector(selectCurrencyIndex);
+
+    const onInputChange = ({ target }) => {
+        console.log(target.value)
+    }
 
     return (
         <Wrapper>
             <Form onChange={(event) => event.preventDefault()}>
-                <Input type="number" min="0.01" step="0.01">
+                <Input type="number" min="0.01" step="0.01" onChange={onInputChange}>
 
                 </Input>
             </Form>
