629e765cd751863b879a20d09ce6c8e4ec097c2b
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-08 19:08:24 +0100

    added useState, const rate, edited onInputChange, added Result

--- src/features/Currency/Calculator/index.js
+++ src/features/Currency/Calculator/index.js
@@ -1,22 +1,28 @@
+import { useState } from "react";
 import { useSelector } from "react-redux"
 import { selectCurrency, selectCurrencyIndex, selectCurrencyStatus } from "../currencySlice"
-import { Form, Wrapper, Input } from "./styled"
+import { Form, Wrapper, Input, Result } from "./styled"
 
 export const Calculator = () => {
     const status = useSelector(selectCurrencyStatus);
     const currency = useSelector(selectCurrency);
     const currencyIndex = useSelector(selectCurrencyIndex);
+    const [result, setResult] = useState(0);
+
+    const rate = status === "success" && currency[19].rates[currencyIndex].mid;
 
     const onInputChange = ({ target }) => {
-        console.log(target.value)
+        const result = target.value * rate;
+        setResult(result);
     }
 
     return (
         <Wrapper>
             <Form onChange={(event) => event.preventDefault()}>
-                <Input type="number" min="0.01" step="0.01" onChange={onInputChange}>
-
-                </Input>
+                <Input type="number" min="0.01" step="0.01" onChange={onInputChange} />
+                <Result>
+                    {result}
+                </Result>
             </Form>
         </Wrapper>
     )
