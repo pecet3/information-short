400462199c097257b56a398864a924380159cef5
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-09 18:00:21 +0100

    added currencyName and currencyCode, created new useState, edited content in Result

--- src/features/Currency/Calculator/index.js
+++ src/features/Currency/Calculator/index.js
@@ -8,22 +8,27 @@ export const Calculator = () => {
     const currency = useSelector(selectCurrency);
     const currencyIndex = useSelector(selectCurrencyIndex);
     const [result, setResult] = useState(0);
+    const [amount, setAmount] = useState(1);
 
     const rate = status === "success" && currency[19].rates[currencyIndex].mid;
+    const currencyName = status === "success" && currency[19].rates[currencyIndex].currency;
+    const currencyCode = status === "success" && currency[19].rates[currencyIndex].code;
 
     const onInputChange = ({ target }) => {
         const result = target.value * rate;
         setResult(result);
+        setAmount(target.value);
     }
 
     return (
         <Wrapper>
             <Form onChange={(event) => event.preventDefault()}>
-                <Input type="number" min="0.01" step="0.01" onChange={onInputChange} />
-                <Result>
-                    {result}
-                </Result>
+                <Input type="number" min="0.01" step="0.01" value={amount} onChange={onInputChange} />
+
             </Form>
+            <Result>
+                {amount} {currencyName} {`(${currencyCode})`} to {result} PLN
+            </Result>
         </Wrapper>
     )
 }
