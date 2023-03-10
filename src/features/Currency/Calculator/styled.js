052a07f4d90361a3c3b051517ca1450a27be9ab9
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-10 20:50:06 +0100

    edited Button and Fieldset

--- src/features/Currency/Calculator/styled.js
+++ src/features/Currency/Calculator/styled.js
@@ -21,16 +21,26 @@ export const Result = styled.p`
 `;
 
 export const Fieldset = styled.fieldset`
-    width: 300px;
+    padding: 20px 26px;  
     border-radius: ${({ theme }) => theme.props.borderRadius}px;
+    background-color: ${({ theme }) => theme.elements.calculator};
 `;
 
 export const Button = styled.button`
+    background-color: ${({ theme }) => theme.elements.buttonBackground};
+    border: none;
+    border-radius: ${({ theme }) => theme.props.borderRadius}px;
+    transition: outline 0.5s;
 
+    &:hover{
+        cursor: pointer;
+        outline: 2px solid ${({ theme }) => theme.elements.primary};
+    }
 `;
 
 export const Legend = styled.legend`
     padding: 4px 8px;
+    background-color: ${({ theme }) => theme.elements.primaryBackground};
     border: 1px solid ${({ theme }) => theme.elements.text};
     border-radius: ${({ theme }) => theme.props.borderRadius}px;
     color: ${({ theme }) => theme.elements.primary};
