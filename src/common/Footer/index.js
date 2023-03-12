89f0aae914ebdca247d89ec873c2c672ea766eb0
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-12 19:20:06 +0100

    edited content

--- src/common/Footer/index.js
+++ src/common/Footer/index.js
@@ -8,6 +8,11 @@ export const Footer = () => {
                 title="link do mojego githuba">
                 Jakub Pacewicz 2023
             </Content>
+            <Content as="a"
+                href="https://github.com/pecet3"
+                title="o stronie">
+                O stronie
+            </Content>
         </Wrapper>
     )
 }
