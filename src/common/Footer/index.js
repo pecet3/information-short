5c5da869e10ebf13b3218c69199ffc16da6bdabc
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-16 19:40:13 +0100

    commentend content

--- src/common/Footer/index.js
+++ src/common/Footer/index.js
@@ -8,11 +8,11 @@ export const Footer = () => {
                 title="link do mojego githuba">
                 Jakub Pacewicz 2023
             </Content>
-            <About>
+            {/* <About>
                 Hej, niezmiernie mi miło, że zajrzałeś na tę stronę.
                 Zrobiłem ją w celu utrwalenia mojej wiedzy z dziedziny frontendu.
                 Wszystkie dane na stronie pochodzą z darmowych API.
-            </About>
+            </About> */}
         </Wrapper>
     )
 }
