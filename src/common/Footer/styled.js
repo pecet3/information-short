3ed5ac27b5cf62be0dce13b3c6abed3ee1fc9909
Jakub Pacewicz <pecet3107@gmail.coim>
2023-03-20 00:40:12 +0100

    removed margin from Wrapper

--- src/common/Footer/styled.js
+++ src/common/Footer/styled.js
@@ -3,7 +3,6 @@ import { ReactComponent as githubIcon } from "./github.svg";
 import { ReactComponent as facebookIcon } from "./facebook.svg";
 
 export const Wrapper = styled.footer`
-    margin: 32px auto 0px auto;
     padding: 8px;
     font-size: 14px;
     background-color: ${({ theme }) => theme.elements.primaryBackground};
